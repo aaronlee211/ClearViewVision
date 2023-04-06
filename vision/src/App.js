fea236a47db77d546d51fc348523643acc381405
Aaron Lee <aaronlee97667@gmail.com>
2023-04-05 22:21:45 -0500

    Updated Home Page with Carousel

--- vision/src/App.js
+++ vision/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import Home from './components/Home.js';
+import Home from './components/Home.js'
 import Nav from './components/Nav.js'
 import Contact from './components/Contact.js'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';

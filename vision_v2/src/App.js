1a209d6122d8df77ec709583846915e73f9d9770
Lee, Kevin <kevinmlee97@gmail.com>
2023-04-17 18:28:30 -0500

    Update favicon

--- vision_v2/src/App.js
+++ vision_v2/src/App.js
@@ -3,6 +3,7 @@ import Header from "./components/Header";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
 import Contact from './components/Contact.js';
+import Reviews from './components/Reviews.js';
 import Doctors from './components/Doctors.js';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
@@ -16,6 +17,7 @@ function App() {
           {/* Multiple Components in a route path can be concatenated in a fragment */}
           <Route path='/' element={<><Header/><Main/></>}/>
           <Route path='contact' element={<Contact />}/>
+          <Route path='reviews' element={<Reviews />}/>
           <Route path='doctors' element={<Doctors />}/>
         </Routes>
       </div>

726219e52f846bf81e77465b02c4b0de3c62b38d
Lee, Kevin <kevinmlee97@gmail.com>
2023-04-17 22:44:24 -0500

    Update Services.js
    -

--- vision_v2/src/components/Services.js
+++ vision_v2/src/components/Services.js
@@ -7,13 +7,13 @@ function Services() {
             <div className="col-md-12">
                 <div className="row services-container">
                     <div className="col-md-4 mb-2">
-                        <Card title="Eye Exams" button="Find Out More" img="img1.png" text="A comprehensive eye exam includes more than just determining your need for glasses or contact lenses. Our doctors look at every aspect of the visual system to determine your ocular health. " />
+                        <Card title="Eye Exams" button="Find Out More" img="img1.png" text="." />
                     </div>
                     <div className="col-md-4 mb-2">
-                        <Card title="Appointments" button="More Information" img="img2.png" text="Schedule your appointment online today! Your personal vision and health needs are very important to us. Our doctors take a holistic approach to help you reach your health and eye care goals." />
+                        <Card title="Appointments" button="More Information" img="img2.png" text="." />
                     </div>
                     <div className="col-md-4 mb-2">
-                        <Card title="Insurance" button="Contact Us" img="img3.png" text="At Hamilton Eye Associates, we accept most vision and medical insurances including Medicare. Contact us at 512-251-4040 or email us here with any questions about your insurance." />
+                        <Card title="Insurance" button="Contact Us" img="img3.png" text="." />
                     </div>
                 </div>
             </div>

a4277511210f47eb3ba6c21c57e8572e108d056e
Aaron Lee <aaronlee97667@gmail.com>
2023-04-17 21:11:11 -0500

    Updated Service Cards

--- vision_v2/src/components/Card.js
+++ vision_v2/src/components/Card.js
@@ -1,19 +1,22 @@
 function Card(props) {
   return (
     <div className="card">
-      <br />
-      
-      <div className="text-center">
-        <img alt="card-img" src={"./img/"+props.img} className="text-center img-fluid" />
+      <img alt="card-img" src={"./img/"+props.img} className="text-center img-fluid prop-image" />
+      <div className="text-center propcard-title">
+        <div className="card-title-grid">
+          <h3 className="card-title">{props.title}</h3>
+        </div>
       </div>
-      <div className="text-center">
-        <h3 className="card-title">{props.title}</h3>
-      </div>
+      <div id="prop-body">
       <div className="p-3">
-        <p className="card-text">
+        <h5 className="card-text">
            {props.text}
-        </p>
+        </h5>
       </div>
+      <div className="center">
+        <button type="button" class="btn btn-info prop-button">{props.button}</button>
+      </div>
+    </div>
     </div>
   );
 }
--- vision_v2/src/components/Services.js
+++ vision_v2/src/components/Services.js
@@ -2,18 +2,18 @@ import Card from "./Card";
 function Services() {
   return (
     <div className="container services">
-        <h2 className="main-title text-center">SERVICES</h2>
+        <h2 className="main-title text-center">Services</h2>
         <div className="card-cover">
             <div className="col-md-12">
-                <div className="row">
+                <div className="row services-container">
                     <div className="col-md-4 mb-2">
-                        <Card title="Web Development" img="card1.png" text="Morbi eget neque risus. Duis erat quam, porta quis enim id, venenatis blandit nunc. " />
+                        <Card title="Eye Exams" button="Find Out More" img="img1.png" text="A comprehensive eye exam includes more than just determining your need for glasses or contact lenses. Our doctors look at every aspect of the visual system to determine your ocular health. " />
                     </div>
                     <div className="col-md-4 mb-2">
-                        <Card title="E-Commerce Services" img="card2.png" text="Maecenas dictum efficitur felis non gravida. Vestibulum vitae ante luctus, accumsan mi vitae, pretium metus." />
+                        <Card title="Appointments" button="More Information" img="img2.png" text="Schedule your appointment online today! Your personal vision and health needs are very important to us. Our doctors take a holistic approach to help you reach your health and eye care goals." />
                     </div>
                     <div className="col-md-4 mb-2">
-                        <Card title="Cyber Security" img="card3.png" text="Phasellus suscipit nibh at nisi finibus vestibulum sit amet vitae massa. Suspendisse non est eget elit pulvinar consectetur nec non sapien." />
+                        <Card title="Insurance" button="Contact Us" img="img3.png" text="At Hamilton Eye Associates, we accept most vision and medical insurances including Medicare. Contact us at 512-251-4040 or email us here with any questions about your insurance." />
                     </div>
                 </div>
             </div>

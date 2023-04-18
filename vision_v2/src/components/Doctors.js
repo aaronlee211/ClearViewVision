09f6ed96ae0dd3c93230f6e951491177e6c2a2c1
Lee, Kevin <kevinmlee97@gmail.com>
2023-04-17 21:11:42 -0500

    Updated Doctors Page

--- vision_v2/src/components/Doctors.js
+++ vision_v2/src/components/Doctors.js
@@ -3,17 +3,30 @@ import React from 'react'
 function Doctors() {
   return (
     <>
-
-        <div class="doctors-header">
-            Meet your doctors from Clearview Vision at Austin, TX!
+        <div class='container-fluid doctors-header'>
+            <div class='col-lg-8 doctors-desc'>
+                <h5>
+                    Our experienced team of optometrists is dedicated to providing you with the highest level of eye care and vision correction services. 
+                    We specialize in comprehensive eye exams to evaluate your overall eye health and provide accurate prescriptions for glasses and contact lenses. 
+                    Our optometrists are highly trained and experienced in the latest technologies and techniques in the field of optometry. 
+                    We are committed to staying up-to-date with the latest research and advancements in eye care to ensure that you receive the best possible treatment. 
+                    Thank you for choosing our optometry practice. We look forward to serving you and your family for all of your eye care needs.
+                </h5>
+            </div>
+            <img class="doctor-header-image" src={require("../images/headerbg3.jpg")}></img>
         </div>
-
         <div class="container-fluid doctor">
             <div class="row doctor-row">
                 <div class="col-lg-7 doctor-text">
                     <div class="text">
                         <h2>Dr. Micaela Lee</h2>
-                        <p>Dr. Micaela Lee is a licensed Optometric Glaucoma specialist with a passion for giving back to the community. She received her Bachelor of Science in Chemistry from The University of Texas at Austin and her Doctorate of Optometry from the University of Houston College of Optometry. Dr. Lee is interested in pediatric optometry & ocular disease, including glaucoma and diabetic retinopathy. She is of Taiwanese descent and fluent in Spanish since she was born in Buenos Aires, Argentina. Dr. Lee enjoys being active, traveling, and spending time with family, friends, and her Labrador named Healy.</p>
+                        <h6>
+                            Dr. Micaela Lee is a licensed Optometric Glaucoma specialist with a passion for giving back to the community. 
+                            She received her Bachelor of Science in Chemistry from The University of Texas at Austin and her Doctorate of Optometry from the University of Houston College of Optometry. 
+                            Dr. Lee is interested in pediatric optometry & ocular disease, including glaucoma and diabetic retinopathy. 
+                            She is of Taiwanese descent and fluent in Spanish since she was born in Buenos Aires, Argentina. 
+                            Dr. Lee enjoys being active, traveling, and spending time with family, friends, and her Labrador named Healy.
+                        </h6>
                     </div>
                 </div>
                 <div class="col-lg-2 doctor-image right">
@@ -32,7 +45,7 @@ function Doctors() {
                 <div class="col-lg-7 doctor-text">
                     <div class="text">
                         <h2>Dr. Micaela Lee</h2>
-                        <p>Dr. Micaela Lee is a licensed Optometric Glaucoma specialist with a passion for giving back to the community. She received her Bachelor of Science in Chemistry from The University of Texas at Austin and her Doctorate of Optometry from the University of Houston College of Optometry. Dr. Lee is interested in pediatric optometry & ocular disease, including glaucoma and diabetic retinopathy. She is of Taiwanese descent and fluent in Spanish since she was born in Buenos Aires, Argentina. Dr. Lee enjoys being active, traveling, and spending time with family, friends, and her Labrador named Healy.</p>
+                        <h6>Dr. Micaela Lee is a licensed Optometric Glaucoma specialist with a passion for giving back to the community. She received her Bachelor of Science in Chemistry from The University of Texas at Austin and her Doctorate of Optometry from the University of Houston College of Optometry. Dr. Lee is interested in pediatric optometry & ocular disease, including glaucoma and diabetic retinopathy. She is of Taiwanese descent and fluent in Spanish since she was born in Buenos Aires, Argentina. Dr. Lee enjoys being active, traveling, and spending time with family, friends, and her Labrador named Healy.</h6>
                     </div>
                 </div>
             </div>

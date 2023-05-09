a299b36de5f5843be2d0f0574abfa616c4406567
Aaron Lee <aaronlee97667@gmail.com>
2023-05-08 20:44:17 -0500

    Header Updated

--- clearviewatx/src/components/Header.jsx
+++ clearviewatx/src/components/Header.jsx
@@ -6,14 +6,14 @@ function Header() {
     <>
     <div className='flex flex-row justify-between my-5'>
       <div>
-        <img className='ml-5'src={require("../images/ClearViewLogo.png")} alt=''></img>
+        <img className='ml-5 header-img'src={require("../images/ClearViewLogo.png")} alt=''></img>
       </div>
-      <div className='flex flex-row items-center mr-5'>
+      <div className='flex flex-row items-center mr-5 header-div'>
           <p className='mr-2 2xl:text-3xl'>Home</p>
           <p className='mr-2 ml-2 2xl:text-3xl'>About</p>
           <p className='mr-2 ml-2 2xl:text-3xl'>Services</p>
           <p className='mr-2 ml-2 2xl:text-3xl'>Contact Us</p>
-          <div className='badge badge-outline badge-lg'>
+          <div className='badge badge-outline badge-lg header-div'>
             <p className='mr-2 ml-2 2xl:text-3xl'>Book an Appointment</p>
           </div>
       </div>

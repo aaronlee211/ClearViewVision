4eeb9e536d4d5d44942e7e11d5394186ac4dae4a
Lee, Kevin <kevinmlee97@gmail.com>
2023-05-11 15:36:58 -0500

    Update footer and extras

--- clearviewatx/src/components/Footer.jsx
+++ clearviewatx/src/components/Footer.jsx
@@ -1,8 +1,55 @@
 import React from 'react'
+import '../css/Footer.css'
+import { ExternalLink } from 'react-external-link';
+import { NavLink } from 'react-router-dom'
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <>
+      <footer className="w-full h-fit footer py-10 px-20" id="footerBackground">
+        <div>
+          <img className='m5-5 footer-img' src={require("../images/ClearViewLogo.png")} alt=''></img>
+          <p className="footerText">
+            &copy; 2023 Clearview Vision ATX.<br />
+            All rights reserved.
+          </p>
+        </div>
+        <div>
+          <span className="footerTitle text-2xl">Resources</span>
+          <a className="link link-hover footerText">Insurance Plans</a>
+          <a className="link link-hover footerText">FAQ's</a>
+          <a className="link link-hover footerText">Customer Reviews</a>
+        </div>
+        <div>
+          <span className="footerTitle text-2xl">About ClearView</span>
+          <NavLink className="link link-hover footerText" to="/about">
+            About Us
+          </NavLink>
+          {/* <a className="">About Us</a> */}
+          <a className="link link-hover footerText">Our Services</a>
+          <a className="link link-hover footerText">Contact Us</a>
+          <a className="link link-hover footerText">Careers</a>
+        </div>
+        <div>
+          <span className="footerTitle text-2xl">Location & Hours</span>
+          <p className="footerText">Address:</p>
+          <ExternalLink className="footerText link link-hover"
+          href="https://www.google.com/maps/place/ClearView+Vision/@30.2328086,-97.8277622,17z/data=!3m1!4b1!4m6!3m5!1s0x865b4be7ae82f12b:0x7f59bfd2b85a9872!8m2!3d30.2328041!4d-97.8228859!16s%2Fg%2F11khzp9p6g">
+            5017 W US Hwy 290, Austin, TX 78735 <br/> (located at Walmart)
+          </ExternalLink>
+          <p className="footerText">
+            Hours: <br/>
+            Sunday: <br/>
+            Monday: <br/>
+            Tuesday: <br/>
+            Wednesday: <br/>
+            Thursday: <br/>
+            Friday: <br/>
+            Saturday: 
+          </p>
+        </div>
+      </footer>
+    </>
   )
 }
 

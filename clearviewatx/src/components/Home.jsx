94f77f01a801c80b57f8c8f0c0b204ee7f7a6cbb
Lee, Kevin <kevinmlee97@gmail.com>
2023-05-26 09:28:09 -0500

    Responsiveness for Card Services

--- clearviewatx/src/components/Home.jsx
+++ clearviewatx/src/components/Home.jsx
@@ -1,10 +1,40 @@
-import React from 'react'
-import '../css/Home.css'  
+import React from 'react';
+import '../css/Home.css';
 import Reviews from './Reviews.jsx';
 import ReviewsReact from './ReviewsReact.jsx';
 import ContactForm from './ContactForm.jsx';
+import { useEffect } from 'react';
 
 function Home() {
+
+  // Setting cards equal heights
+  useEffect(() => {
+    const setEqualCardHeights = () => {
+      const cards = document.querySelectorAll(".card p");
+      let maxHeight = 0;
+
+      Array.from(cards).forEach((card) => {
+        const cardHeight = card.offsetHeight;
+        if (cardHeight > maxHeight) {
+          maxHeight = cardHeight;
+        }
+      });
+
+      Array.from(cards).forEach((card) => {
+        card.style.height = `${maxHeight}px`;
+      });
+
+      console.log(`${maxHeight}`)
+    };
+
+    window.addEventListener('load', setEqualCardHeights);
+    // Make sure to remove the event listener when the component unmounts
+    return () => {
+      window.removeEventListener('load', setEqualCardHeights);
+    };
+  }, []);
+
+
   return (
     <>
       {/* Banner */}
@@ -39,51 +69,46 @@ function Home() {
       </div>
 
       {/* Cards of Services */}
-      <div className="flex justify-between mx-auto 2xl:h-128" id="serviceCards">
 
-        <div className="sm:w-1/3 flex items-center">
-          <div className="max-w-md max-h-64 rounded overflow-hidden shadow-lg mx-auto">
-            <div className="px-6 py-4">
+      <div className="lg:flex lg:justify-between md:mx-auto h-fit lg:py-20 py-14" id="serviceCards">
+
+        <div className="lg:w-1/3 w-full flex items-center my-4">
+          <div className="max-w-md h-fit rounded overflow-hidden shadow-lg mx-auto">
+            <div className="card px-6 py-4">
               <div className="font-bold poppins cardTitle text-center text-2xl mb-4">Eye Exams</div>
               <p className="poppins cardText text-center">
                 Get a comprehensive eye exam from our experienced optometrists to prolong your vision and eye health.
               </p>
-            </div>
-            <div className="px-6 py-4 flex justify-center">
-              <button className="py-2 px-4 rounded-full cardButtons">
+              <button className="my-4 mx-14 py-2 rounded-full cardButtons">
                 View Services
               </button>
             </div>
           </div>
         </div>
 
-        <div className="sm:w-1/3 flex items-center">
+        <div className="lg:w-1/3 w-full flex items-center my-4">
           <div className="max-w-md max-h-64 rounded overflow-hidden shadow-lg mx-auto">
-            <div className="px-6 py-4">
+            <div className="card px-6 py-4">
               <div className="font-bold poppins cardTitle text-center text-2xl mb-4">Appointments</div>
               <p className="poppins cardText text-center">
                 Schedule an appointment for an eye exam, contact lens fitting, or other eye care services at our convenient location.
               </p>
-            </div>
-            <div className="px-6 py-4 flex justify-center">
-              <button className="py-2 px-4 rounded-full cardButtons flex items-end">
+              <button className="my-4 mx-14 py-2 rounded-full cardButtons">
                 Book an Appointment
               </button>
             </div>
           </div>
         </div>
-
-        <div className="sm:w-1/3 flex items-center">
+        
+        <div className="lg:w-1/3 w-full flex items-center my-4">
           <div className="max-w-md max-h-64 rounded overflow-hidden shadow-lg mx-auto">
-            <div className="px-6 py-4">
+            <div className="card px-6 py-4">
               <div className="font-bold poppins cardTitle text-center text-2xl mb-4">Insurance</div>
               <p className="poppins cardText text-center">
                 We accept most insurance plans to help you save money on your eye care services.
                 Contact us to learn more about your coverage options.
               </p>
-            </div>
-            <div className="px-6 py-4 flex justify-center relative">
-              <button className="py-2 px-4 rounded-full cardButtons">
+              <button className="my-4 mx-14 py-2 rounded-full cardButtons">
                 View Insurance Plans
               </button>
             </div>
@@ -93,10 +118,10 @@ function Home() {
       </div>
 
       {/* Reviews Carousel */}
-      <ReviewsReact/>
+      <ReviewsReact />
 
       {/* Contact Us Form */}
-      <ContactForm/>
+      <ContactForm />
     </>
   )
 }
